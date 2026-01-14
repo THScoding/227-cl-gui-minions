786b40d13dd18bb889d5b666c62ef0ca88a29341
Harry Marshall <harry.marshall@crpusd.org>
2026-01-14 09:43:57 -0800

    more general config

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -4,12 +4,11 @@ import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename    
 def do_command(command):
-    command = ["ping", "local_host", "-n", "4"]
+    command = ["ping", "localhost", "-n", "4"]
     # Windows version to limit to 4 requests: command = ["ping", "localhost", "-n", "4"]
     # Mac version to limit to 4 requests:     command = ["ping", "localhost", "-n", "4"]
         
     subprocess.run(command)
-    # 1. Create the main application window
     global command_textbox, url_entry
     
     command_textbox.delete(1.0, tk.END)
@@ -20,10 +19,9 @@ def do_command(command):
         for line in p.stdout:
             command_textbox.insert(tk.END,line)
             command_textbox.update()
-    # CODE TO ADD
-    # Modify the do_command(command) function: 
-    # to use the text box for input to the functions 
-
+# CODE TO ADD
+# Modify the do_command(command) function: 
+# to use the text box for input to the functions
         # If url_entry is blank, use localhost IP address 
     url_val = url_entry.get()
     if (len(url_val) == 0):
@@ -42,43 +40,40 @@ def mSave():
     
 window = tk.Tk()
 window.title("ip config")
-for i in range(3):
-    window.columnconfigure(i, weight=1, minsize=75)
-    window.rowconfigure(i, weight=1, minsize=50)
 
-    for j in range(0, 5):
-        frame = tk.Frame(
-            master=window,
-            relief=tk.RAISED,
-            borderwidth=1
-        )
-        ping_btn = tk.Button(master=window, text="ping", command=do_command)
-        ping_btn.grid(row=0,column=0,sticky="nw")
+ping_btn = tk.Button(master=window, text="ping", command=do_command)
+ping_btn.pack()
         
                 # creates the frame with label for the text box
-        frame_URL = tk.Frame(window, pady=10,  bg="black") # change frame color
-        frame_URL.grid(row=1,column=1)
+frame_URL = tk.Frame(window, pady=10,  bg="black") # change frame color
+frame_URL.pack()
 
-        # decorative label
-        url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
-            compound="center",
-            font=("comic sans", 14),
-            bd=0, 
-            relief=tk.FLAT, 
-            cursor="heart",
-            fg="mediumpurple3",
-            bg="black")
-        url_label.pack(side=tk.LEFT)
-        url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
-        url_entry.pack(side=tk.LEFT)
-
-        frame = tk.Frame(window,  bg="black") # change frame color
-        frame.grid(row=1,column=1)
-        # Adds an output box to GUI.
-        command_textbox = tksc.ScrolledText(window, height=10, width=100)
-        command_textbox.grid(row=2,column=0)
-        #url check button
-        ping_btn = tk.Button(window, text="Check to see if a URL is up and active", command=lambda:do_command("ping"))
-        ping_btn.grid(row=0,column=0,sticky="n")
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+    compound="center",
+    font=("comic sans", 14),
+    bd=0, 
+    relief=tk.FLAT, 
+    cursor="heart",
+    fg="mediumpurple3",
+    bg="black")
+url_label.pack()
+frame = tk.Frame(window,  bg="black") # change frame color
+frame.pack(side=tk.LEFT)
+# Adds an output box to GUI.
+command_textbox = tksc.ScrolledText(window, height=10, width=100)
+command_textbox.pack()
+#url check button
+# CODE TO ADD
+# Modifies the ping button parameters.
+ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", 
+    command=lambda:do_command("ping"),
+    compound="center",
+    font=("comic sans", 12),
+    bd=0, 
+    relief="flat",
+    cursor="heart",
+    bg="white", activebackground="gray")
+ping_btn.pack(side=tk.RIGHT) 
 
 window.mainloop()
