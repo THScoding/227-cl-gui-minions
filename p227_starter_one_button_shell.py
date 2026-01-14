c30207c03fd7812fc58e752e4d2321eb6b638adb
Harry Marshall <harry.marshall@crpusd.org>
2026-01-14 09:18:34 -0800

    command stuff

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -2,15 +2,43 @@ import subprocess
 import tkinter as tk
 import tkinter.scrolledtext as tksc
 from tkinter import filedialog
-from tkinter.filedialog import asksaveasfilename
-
-def do_command():
-    command = ["ping", "localhost", "-n", "4"]
+from tkinter.filedialog import asksaveasfilename    
+def do_command(command):
+    command = ["ping", "local_host", "-n", "4"]
     # Windows version to limit to 4 requests: command = ["ping", "localhost", "-n", "4"]
     # Mac version to limit to 4 requests:     command = ["ping", "localhost", "-n", "4"]
-    
+        
     subprocess.run(command)
     # 1. Create the main application window
+    global command_textbox, url_entry
+    
+    command_textbox.delete(1.0, tk.END)
+    command_textbox.insert(tk.END, command + " working....\n")
+    command_textbox.update()
+
+    with subprocess.Popen(command + ' ' + url_val, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            command_textbox.insert(tk.END,line)
+            command_textbox.update()
+    # CODE TO ADD
+    # Modify the do_command(command) function: 
+    # to use the text box for input to the functions 
+
+        # If url_entry is blank, use localhost IP address 
+    url_val = url_entry.get()
+    if (len(url_val) == 0):
+        # url_val = "127.0.0.1"
+        url_val = "::1"
+
+def mSave():
+  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+  if filename is None:
+    return
+  file = open (filename, mode = 'w')
+  text_to_save = command_textbox.get("1.0", tk.END)
+  
+  file.write(text_to_save)
+  file.close()
     
 window = tk.Tk()
 window.title("ip config")
@@ -25,7 +53,7 @@ for i in range(3):
             borderwidth=1
         )
         ping_btn = tk.Button(master=window, text="ping", command=do_command)
-        ping_btn.grid(row=0,column=0,sticky="n")
+        ping_btn.grid(row=0,column=0,sticky="nw")
         
                 # creates the frame with label for the text box
         frame_URL = tk.Frame(window, pady=10,  bg="black") # change frame color
@@ -51,6 +79,6 @@ for i in range(3):
         command_textbox.grid(row=2,column=0)
         #url check button
         ping_btn = tk.Button(window, text="Check to see if a URL is up and active", command=lambda:do_command("ping"))
-        ping_btn.grid(row=0,column=0,sticky="S")
+        ping_btn.grid(row=0,column=0,sticky="n")
 
 window.mainloop()
