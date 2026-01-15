cf4fea65dd83ddf7d30dc3343d2f8e51e8b5df4e
Harry Marshall <harry.marshall@crpusd.org>
2026-01-15 09:39:49 -0800

    redid the whole thing

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -3,12 +3,10 @@ import tkinter as tk
 import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename    
+
+def do_command():
+    subprocess.call("ping localhost")
 def do_command(command):
-    command = ["ping", "localhost", "-n", "4"]
-    # Windows version to limit to 4 requests: command = ["ping", "localhost", "-n", "4"]
-    # Mac version to limit to 4 requests:     command = ["ping", "localhost", "-n", "4"]
-        
-    subprocess.run(command)
     global command_textbox, url_entry
     
     command_textbox.delete(1.0, tk.END)
@@ -19,33 +17,44 @@ def do_command(command):
         for line in p.stdout:
             command_textbox.insert(tk.END,line)
             command_textbox.update()
-# CODE TO ADD
-# Modify the do_command(command) function: 
-# to use the text box for input to the functions
-        # If url_entry is blank, use localhost IP address 
+# If url_entry is blank, use localhost IP address 
     url_val = url_entry.get()
     if (len(url_val) == 0):
         # url_val = "127.0.0.1"
         url_val = "::1"
 
 def mSave():
-  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
-  if filename is None:
-    return
-  file = open (filename, mode = 'w')
-  text_to_save = command_textbox.get("1.0", tk.END)
-  
-  file.write(text_to_save)
-  file.close()
-    
+    filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+    if filename is None:
+        return
+    file = open (filename, mode = 'w')
+    text_to_save = command_textbox.get("1.0", tk.END)
+    file.write(text_to_save)
+    file.close()
+
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-ping_btn = tk.Button(root, text="ping", command=do_command)
+# set up button to run the do_command function
+ping_btn = tk.Button(frame, text="ping", command=do_command)
 ping_btn.pack()
-        
-                # creates the frame with label for the text box
+# CODE TO ADD
+# Makes the command button pass it's name to a function using lambda
+ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", 
+    command=lambda:do_command("ping"),
+    compound="center",
+    font=("comic sans", 12),
+    bd=0, 
+    relief="flat",
+    cursor="heart",
+    bg="white", activebackground="gray")
+ping_btn.pack() 
+#save button
+ping_btn = tk.Button(frame, text="Save output", command=mSave)
+ping_btn.pack()
+
+# creates the frame with label for the text box
 frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
 frame_URL.pack()
 
@@ -58,23 +67,14 @@ url_label = tk.Label(frame_URL, text="Enter a URL of interest: ",
     cursor="heart",
     fg="mediumpurple3",
     bg="black")
-url_label.pack()
+url_label.pack(side=tk.LEFT)
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry.pack(side=tk.LEFT)
+
 frame = tk.Frame(root,  bg="black") # change frame color
-frame.pack(side=tk.LEFT)
+frame.pack()
 # Adds an output box to GUI.
-command_textbox = tksc.ScrolledText(root, height=10, width=100)
+command_textbox = tksc.ScrolledText(frame, height=10, width=100)
 command_textbox.pack()
-#url check button
-# CODE TO ADD
-# Modifies the ping button parameters.
-ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", 
-    command=lambda:do_command("ping"),
-    compound="center",
-    font=("comic sans", 12),
-    bd=0, 
-    relief="flat",
-    cursor="heart",
-    bg="white", activebackground="gray")
-ping_btn.pack(side=tk.RIGHT) 
 
 root.mainloop()
